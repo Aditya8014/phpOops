2bc2029e601d9991d96250c708c2940b71e24aa0
Aditya8014 <adityapk60@gmail.com>
2020-11-10 07:12:11 +0545

    Method Test by Creating class and object

---
+++ ObjectClass_Demo.php
@@ -0,0 +1,12 @@
+<?php  
+class demo  
+{  
+        private $a= "Method Test";  
+        public function display()  
+        {  
+        echo $this->a;  
+        }  
+}  
+$obj = new demo();  
+    $obj->display();  
+?>  
